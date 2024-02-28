7dde32a1a90f31558729c946bfe9a040dc8900bc
Agus N <agus_sabia@hotmail.com>
2024-02-28 18:47:29 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/ClienteController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Cliente> ObtenerClientes()
+        {
+            string sRet = "";
+            List<Models.Cliente> ListaClientes = (List<Cliente>)Cliente.ObtenerClientes(ref sRet);
+
+            return ListaClientes;
+        }
+
+        [HttpGet]
+        public Cliente ObtenerClienteID(int id)
+        {   
+            
+            string sRet = "";
+            Models.Cliente cliente = (Cliente)Cliente.ObtenerClienteID(id, out sRet);
+
+            return cliente;
+        }
+
+        [HttpGet]
+        public Cliente ObtenerClientePorNombre(string nombre)
+        {
+
+            string sResult = "";
+            Models.Cliente cliente = (Cliente)Cliente.ObtenerClientePorNombre(nombre, out sResult);
+
+            return cliente;
+        }
+
+        [HttpPost]
+
+        [Route("api/Cliente/insertar")]
+        public IHttpActionResult InsertarCliente([FromBody] Models.Cliente cliente)
+        {
+
+            
+            string mensaje = Cliente.InsertarCliente(cliente);
+
+
+            return Ok(mensaje);
+        }
+        [HttpPost]
+        [Route("api/Cliente/actualizar")]
+        public IHttpActionResult ActualizarCliente([FromBody] Models.Cliente cliente)
+        {
+            string mensaje = Cliente.ActualizarCliente(cliente);
+            return Ok(mensaje);
+        }
+    }
+}
---
+++ WebApplication1/Models/Cliente.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.UI.WebControls;
+using System.Web.Http.Results;
+
+namespace WebApplication1.Models
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }  
+        public string Apellido { get; set; }
+
+        public DateTime FechaDeNacimiento { get; set; }
+
+        public string CUIT { get; set; }
+        public string Domicilio { get; set; }
+        public string TelefonoCelular { get; set; }
+        public string Email { get; set; }
+
+        public static IEnumerable<Cliente> ObtenerClientes(ref string sResult)
+        {
+            SqlConnection MyConnection = default(SqlConnection);
+            SqlDataAdapter MyDataAdapter = default(SqlDataAdapter);
+            try
+            {
+                MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringSQL"].ConnectionString);
+                MyDataAdapter = new SqlDataAdapter("spObtenerClientes", MyConnection);
+                MyDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                DataTable dt = new DataTable();
+                MyDataAdapter.Fill(dt);
+
+                List<Models.Cliente> ListaClientes = new List<Models.Cliente>();
+                foreach (DataRow row in dt.Rows)
+                {
+                    var Cliente = new Cliente();
+                    Cliente.ID = int.Parse(row["ID"].ToString());
+                    Cliente.Nombre = row["Nombres"].ToString();
+                    Cliente.Apellido = row["Apellidos"].ToString();
+                    Cliente.FechaDeNacimiento = DateTime.Parse(row["FechaDeNacimiento"].ToString());
+                    Cliente.CUIT = row["CUIT"].ToString();
+                    Cliente.Domicilio = row["Domicilio"].ToString();
+                    Cliente.TelefonoCelular = row["TelefonoCelular"].ToString();
+                    Cliente.Email = row["Email"].ToString();
+
+                    ListaClientes.Add(Cliente);
+                }
+
+                sResult = "";
+                return ListaClientes;   
+
+            }catch(Exception ex)
+            {
+                sResult = ex.Message;
+                return null;
+            }
+
+        }
+        public static Cliente ObtenerClienteID(int id, out string sResult)
+        {
+            SqlConnection MyConnection = default(SqlConnection);
+            SqlDataAdapter MyDataAdapter = default(SqlDataAdapter);
+            try
+            {
+                MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringSQL"].ConnectionString);
+                MyDataAdapter = new SqlDataAdapter("spObtenerClienteID", MyConnection);
+                MyDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                MyDataAdapter.SelectCommand.Parameters.AddWithValue("@id", id);
+                
+
+                DataTable dt = new DataTable();
+                MyDataAdapter.Fill(dt);
+
+                var Cliente = new Cliente();
+
+                if (dt.Rows.Count == 0)
+                {
+                    sResult = "No se encontró un resultado";
+                    return null;
+                }
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    Cliente.ID = int.Parse(row["ID"].ToString());
+                    Cliente.Nombre = row["Nombres"].ToString();
+                    Cliente.Apellido = row["Apellidos"].ToString();
+                    Cliente.FechaDeNacimiento = DateTime.Parse(row["FechaDeNacimiento"].ToString());
+                    Cliente.CUIT = row["CUIT"].ToString();
+                    Cliente.Domicilio = row["Domicilio"].ToString();
+                    Cliente.TelefonoCelular = row["TelefonoCelular"].ToString();
+                    Cliente.Email = row["Email"].ToString();
+
+                }
+
+                sResult = "";
+                return Cliente;
+
+            }
+            catch (Exception ex)
+            {
+                sResult = ex.Message;
+                return null;
+            }
+
+        }
+        public static Cliente ObtenerClientePorNombre(string nombre, out string sResult)
+        {
+            SqlConnection MyConnection = default(SqlConnection);
+            SqlDataAdapter MyDataAdapter = default(SqlDataAdapter);
+            try
+            {
+                MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringSQL"].ConnectionString);
+                MyDataAdapter = new SqlDataAdapter("spBuscarPorNombre", MyConnection);
+                MyDataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                MyDataAdapter.SelectCommand.Parameters.AddWithValue("@nombre", nombre);
+
+
+                DataTable dt = new DataTable();
+                MyDataAdapter.Fill(dt);
+
+                var Cliente = new Cliente();
+
+                if (dt.Rows.Count == 0)
+                {
+                    sResult = "No se encontró un resultado";
+                    return null;
+                }
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    Cliente.ID = int.Parse(row["ID"].ToString());
+                    Cliente.Nombre = row["Nombres"].ToString();
+                    Cliente.Apellido = row["Apellidos"].ToString();
+                    Cliente.FechaDeNacimiento = DateTime.Parse(row["FechaDeNacimiento"].ToString());
+                    Cliente.CUIT = row["CUIT"].ToString();
+                    Cliente.Domicilio = row["Domicilio"].ToString();
+                    Cliente.TelefonoCelular = row["TelefonoCelular"].ToString();
+                    Cliente.Email = row["Email"].ToString();
+
+                }
+
+                sResult = "";
+                return Cliente;
+
+            }
+            catch (Exception ex)
+            {
+                sResult = ex.Message;
+                return null;
+            }
+
+        }
+
+        public static string InsertarCliente(Models.Cliente cliente)
+        {
+            string sResult = "";
+            SqlConnection MyConnection = default(SqlConnection);
+            SqlCommand MySqlCommand = default(SqlCommand);
+            try
+            {
+                MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringSQL"].ConnectionString);
+                MySqlCommand = new SqlCommand("spInsertarCliente", MyConnection);
+                MySqlCommand.CommandType = CommandType.StoredProcedure;
+
+                MySqlCommand.Parameters.AddWithValue("@ID", 0);
+                MySqlCommand.Parameters.AddWithValue("@Nombres", cliente.Nombre);
+                MySqlCommand.Parameters.AddWithValue("@Apellidos", cliente.Apellido);
+                MySqlCommand.Parameters.AddWithValue("@FechaDeNacimiento", cliente.FechaDeNacimiento);
+                MySqlCommand.Parameters.AddWithValue("@CUIT", cliente.CUIT);
+                MySqlCommand.Parameters.AddWithValue("@Domicilio", cliente.Domicilio);
+                MySqlCommand.Parameters.AddWithValue("@TelefonoCelular", cliente.TelefonoCelular);
+                MySqlCommand.Parameters.AddWithValue("@Email", cliente.Email);
+            
+                MyConnection.Open();
+                MySqlCommand.ExecuteNonQuery();
+
+                MyConnection.Close();
+                MyConnection.Dispose();
+                
+
+                
+
+                sResult = "Cliente Creado con exito";
+                
+
+            }
+            catch (Exception ex)
+            {
+                sResult = ex.Message;
+                return null;
+            }
+            return sResult;
+        }
+        public static string ActualizarCliente(Models.Cliente cliente)
+        {
+            string sResult = "";
+            SqlConnection MyConnection = default(SqlConnection);
+            SqlCommand MySqlCommand = default(SqlCommand);
+            try
+            {
+                MyConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringSQL"].ConnectionString);
+                MySqlCommand = new SqlCommand("spActualizarCliente", MyConnection);
+                MySqlCommand.CommandType = CommandType.StoredProcedure;
+
+                MySqlCommand.Parameters.AddWithValue("@ID", cliente.ID);
+                MySqlCommand.Parameters.AddWithValue("@Nombres", cliente.Nombre);
+                MySqlCommand.Parameters.AddWithValue("@Apellidos", cliente.Apellido);
+                MySqlCommand.Parameters.AddWithValue("@FechaDeNacimiento", cliente.FechaDeNacimiento);
+                MySqlCommand.Parameters.AddWithValue("@CUIT", cliente.CUIT);
+                MySqlCommand.Parameters.AddWithValue("@Domicilio", cliente.Domicilio);
+                MySqlCommand.Parameters.AddWithValue("@TelefonoCelular", cliente.TelefonoCelular);
+                MySqlCommand.Parameters.AddWithValue("@Email", cliente.Email);
+
+                MyConnection.Open();
+                MySqlCommand.ExecuteNonQuery();
+
+                MyConnection.Close();
+                MyConnection.Dispose();
+
+
+
+
+                sResult = "Cliente Modificado con exito";
+
+
+            }
+            catch (Exception ex)
+            {
+                sResult = ex.Message;
+                return null;
+            }
+            return sResult;
+        }
+
+    }
+
+
+}
